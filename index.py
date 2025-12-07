8249ec2223f87832f9173602774f2e54ce3602d1
FahadAliHashmi2011 <faiezali2006@gmail>
2025-12-07 15:26:02 +0000

    ss

--- index.py
+++ index.py
@@ -21,3 +21,35 @@ print(ob1 < ob2)
 ob3= A(4)
 ob4= A(5)
 print(ob3 == ob4)
+###################################################
+class flashcard:
+    def __init__(self, word, meaning):
+        self.word = word
+        self.meaning = meaning
+    def __str__(self):
+        return self.word + ' (' + self.meaning + ')'
+
+flash = []
+print("welcome to flashcard application")
+while(True):
+    word = input("enter the name you want to add to flashcard : ")
+    meaning = input("enter the meaning of the word : ")
+    
+    flash.append(flashcard(word, meaning))
+    
+    option = int(input("enter 0 , if you want to add another flashcard otherwise enter 1 : "))
+    
+    if(option):
+        break
+
+print("\nYour flashcards")
+for i in flash:
+    print(">", i)
+
+
+
+
+
+
+
+    
